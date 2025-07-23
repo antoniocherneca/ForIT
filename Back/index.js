76c2af39d055c0e2a434e2efb33fb43afb7b8476
antoniocherneca <antumaster09@hotmail.com>
2025-07-23 02:32:48 -0300

    Agregué los métodos post y delete. Además también agregué put pero no funciona todavía

--- Back/index.js
+++ Back/index.js
@@ -1,10 +1,11 @@
-import { log } from "console";
 import express from "express";
 import fs from "fs";
-import { json } from "stream/consumers";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.json());
 
 const readData = () => {
   try {
@@ -29,15 +30,73 @@ app.get("/", (req, res) => {
 
 app.get("/api/tasks", (req, res) => {
   const data = readData();
-
-  res.json(data.tasks);
+  if (data.tasks.length > 0) {
+    res.json(data.tasks);
+  } else {
+    res.send("No se encontraron tareas");
+  }
 });
 
-app.get("api/tasks/:id", (req, res) => {
-  const data = readData();
-  const id = parseInt(req.params.id);
-  const task = data.tasks.find((task) => task.id === id);
-  res.json(task);
+app.post("/api/tasks", (req, res) => {
+  try {
+    const data = readData();
+    const maxId = data.tasks[data.tasks.length - 1].id;
+    const newTask = {
+      id: maxId + 1,
+      title: req.body.title,
+      description: req.body.description,
+      completed: false,
+    };
+    data.tasks.push(newTask);
+    writeData(data);
+    res.json(newTask);
+  } catch (error) {
+    console.log(error);
+    res.send("Error al agregar la tarea");
+  }
+});
+
+app.put("/api/tasks/:id", (req, res) => {
+  try {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const taskIndex = data.tasks.findIndex((task) => task.id === id);
+
+    if (taskIndex === -1) {
+      return res.send("Tarea no encontrada");
+    }
+
+    const updatedTask = {
+      ...data.tasks[taskIndex],
+      title: req.body.title || data.tasks[taskIndex].title,
+      description: req.body.description || data.tasks[taskIndex].description,
+      completed:
+        req.body.completed !== undefined
+          ? req.body.completed
+          : data.tasks[taskIndex].completed,
+    };
+
+    data.tasks[taskIndex] = updatedTask;
+    writeData(data);
+    res.json(updatedTask);
+  } catch (error) {
+    console.log(error);
+    res.send("Error al actualizar la tarea");
+  }
+});
+
+app.delete("/api/tasks/:id", (req, res) => {
+  try {
+    const data = readData();
+    const id = parseInt(req.params.id);
+    const undeletedTasks = data.tasks.filter((task) => task.id !== id);
+    data.tasks = undeletedTasks;
+    writeData(data);
+    res.json(undeletedTasks);
+  } catch (error) {
+    console.log(error);
+    res.send("Error al eliminar la tarea");
+  }
 });
 
 app.listen(PORT, () => {
